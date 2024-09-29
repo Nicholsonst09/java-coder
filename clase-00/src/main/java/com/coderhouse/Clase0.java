039229c7aa75ba89f24b429ece7a694cfa434473
NichoStrick <nicholsonstrickland@gmail.com>
2024-09-29 14:05:08 -0300

    Hello world-First lines

---
+++ clase-00/src/main/java/com/coderhouse/Clase0.java
@@ -0,0 +1,10 @@
+package com.coderhouse;
+
+public class Clase0 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("First day in Java");
+	}
+
+}
